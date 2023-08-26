d97a1743923c376df1faf4dd59085ae243220ef8
sarabdev <sarab@contrivers.dev>
2023-08-26 15:32:32 +0500

    stripe added-9

--- netlify/functions/identity-signup.js
+++ netlify/functions/identity-signup.js
@@ -1,7 +1,17 @@
-exports.handler = function(event, context, callback) {
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async function(event, context, callback) {
   const data = JSON.parse(event.body);
   const { user } = data;
-    
+  // create a new customer in Stripe
+   const customer = await stripe.customers.create({ email: user.email, name:user.name });
+
+  // // subscribe the new customer to the free plan
+  await stripe.subscriptions.create({
+    customer: customer.id,
+    items: [{ price: 'price_1NjFlHEpEL3jFdarAGB6XUBh'}],
+  });
+
   const responseBody = {
     app_metadata: {
       roles: ["registered"]
