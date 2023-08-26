bee8759b073aab35ee8f2905b24b9c1689af02b1
sarabdev <sarab@contrivers.dev>
2023-08-26 11:36:03 +0500

    stripe added-3

--- netlify/functions/identity-signup.js
+++ netlify/functions/identity-signup.js
@@ -1,5 +1,20 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-const { faunaFetch } = require('./utils/fauna');
+const fetch = require('node-fetch');
+
+const faunaFetch = async ({ query, variables }) => {
+    return await fetch('https://graphql.fauna.com/graphql', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${process.env.FAUNA_SERVER_KEY}`,
+      },
+      body: JSON.stringify({
+        query,
+        variables,
+      }),
+    })
+      .then((res) => res.json())
+      .catch((err) => console.error(JSON.stringify(err, null, 2)));
+  };
 
 exports.handler = async (event) => {
   const { user } = JSON.parse(event.body);
