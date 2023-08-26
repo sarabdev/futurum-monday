b4ade4fd6d4755ec2716e52cb7995bd83408b2e4
sarabdev <sarab@contrivers.dev>
2023-08-26 21:54:17 +0500

    latest2

--- netlify/functions/identity-signup.js
+++ netlify/functions/identity-signup.js
@@ -1,16 +1,16 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')("sk_test_51Na6KqEpEL3jFdarNkEe9kJENhhwpDY2R4u9IrS6mrDrNHFN8zABC79u8SRE5aEpzjhtNhSpwBw5tw4i7OzBRQtX00Hwr1Armb");
 
 exports.handler = async function(event, context, callback) {
   const data = JSON.parse(event.body);
   const { user } = data;
   // create a new customer in Stripe
-   const customer = await stripe.customers.create({ email: user.email, name:user.name });
+  //  const customer = await stripe.customers.create({ email: user.email, name:user.name });
 
-  // // subscribe the new customer to the free plan
-  await stripe.subscriptions.create({
-    customer: customer.id,
-    items: [{ price: process.env.STRIPE_DEFAULT_PRICE_PLAN}],
-  });
+  // // // subscribe the new customer to the free plan
+  // await stripe.subscriptions.create({
+  //   customer: customer.id,
+  //   items: [{ price:"price_1NjFlHEpEL3jFdarAGB6XUBh"}],
+  // });
 
   return {
     statusCode: 200,
