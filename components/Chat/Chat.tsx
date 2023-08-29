bbd0ae4952ec7ae8d81d7a649fd3b2c483433572
sarabdev <sarab@contrivers.dev>
2023-08-30 00:25:45 +0500

    demo900

--- components/Chat/Chat.tsx
+++ components/Chat/Chat.tsx
@@ -384,6 +384,10 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
     if(process.env.NEXT_PUBLIC_API_KEY)
     localStorage.setItem('apiKey', process.env.NEXT_PUBLIC_API_KEY);
    }
+   else{
+    homeDispatch({ field: 'apiKey', value:'' });
+    localStorage.setItem('apiKey', '');
+   }
   },[user])
 
   return (
@@ -392,7 +396,7 @@ export const Chat = memo(({ stopConversationRef }: Props) => {
       color: lightMode=="light" ? "black" : "white",
       borderColor: lightMode=="light" ? "black" : "white"
     }}  className="relative flex-1 overflow-hidden bg-black dark:bg-[#343541]">
-      {!(user || apiKey || serverSideApiKeyIsSet) ? (
+      {!(apiKey || serverSideApiKeyIsSet)  ? (
         <div className="mx-auto flex h-full w-[300px] flex-col justify-center space-y-6 sm:w-[600px]">
           <div className="text-center text-4xl font-bold text-black dark:text-white">
           Futurum One
